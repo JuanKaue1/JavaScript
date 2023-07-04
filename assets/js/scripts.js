39e4c01a836964fc879d8ecb79b53da1a15ff3fb
Juan Kauê <106188053+JuanKaue1@users.noreply.github.com>
2023-07-04 10:36:43 -0300

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,49 @@
-const js_icon = document.querySelector('#js_icon');
+const js_icon = document.querySelector('#js_icon_a');
 const container_js = document.querySelector('.container-js');
+let isRotating = false;
 
 container_js.addEventListener('click', () => {
-  if (js_icon.style.display !== 'none') {
-    js_icon.style.animation = 'identifier 2s linear infinite';
-    js_icon.href = './sintaxe_e_estrutura_do_javascript.html"';
+  if (isRotating) {
+    js_icon.style.animation = 'none';
+    isRotating = false;
+  } else {
+    js_icon.style.animation = 'rotateAnimation 2s linear infinite';
+    isRotating = true;
   }
 });
+
+
+
+
+
+
+function outerFunction() {
+  let outerVariable = 'Valor externo';
+
+  function innerFunction() {
+    console.log(outerVariable); // Acessa a variável definida no escopo superior
+  }
+
+  innerFunction();
+}
+
+outerFunction(); // Saída: 'Valor externo'
+
+
+
+
+let globalVariable = 'Valor global';
+
+function globalFunction() {
+  console.log(globalVariable); // Acessa a variável global
+}
+
+globalFunction(); // Saída: 'Valor global']
+
+function localFunction() {
+  let localVariable = 'Valor local';
+  console.log(localVariable); // Acessa a variável local
+}
+
+localFunction(); // Saída: 'Valor local'
+console.log(localVariable); // Erro: localVariable não está definido
